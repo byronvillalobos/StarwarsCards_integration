475b4376c2d7237a1b9cffc9ffa5599ff0d541d6
byronvillalobos <byronvillalobos@hotmail.com>
2021-03-17 03:56:12 +0000

    comments

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,28 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.scss";
+import { Link, useParams } from "react-router-dom";
+import "/workspace/react-hello-webapp/src/styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container text-center mt-5 bg'dark">
+		<div className="row">
+			<div className="col-md-6 d-flex justify-content-center">
+				<Link to="/people">
+					<img
+						className="d-block"
+						src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/star-wars-characters-ranked-1577122930.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*"
+						alt="People"
+					/>
+				</Link>
+			</div>
+			<div className="col-md-6 d-flex justify-content-center">
+				<Link to="/planets">
+					<img
+						className="d-block"
+						src="https://preview.redd.it/ftamwl8y4m131.jpg?width=640&crop=smart&auto=webp&s=c674a1fde1d1395cf80d896c6ece6450dca606f8"
+						alt="Planets"
+					/>
+				</Link>
+			</div>
+		</div>
 	</div>
 );
